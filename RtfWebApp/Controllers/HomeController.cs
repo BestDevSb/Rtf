9e846f37fed158e933583f277514b917129a83f8
mk <mk@MKPC>
2018-11-01 17:58:51 +0300

    Add user list, user info and test data

--- RtfWebApp/Controllers/HomeController.cs
+++ RtfWebApp/Controllers/HomeController.cs
@@ -5,12 +5,83 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RtfWebApp.Models;
+using RtfWebApp.Models.View;
 
 namespace RtfWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private static Random _rnd = new Random();
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            List<UserViewModel> result = new List<UserViewModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                result.Add(new UserViewModel
+                {
+                    Id = i,
+                    Name = "Test user name " + i,
+                    AvatarId = i
+                });
+            }
+            return View(result);
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult User(int id)
+        {
+            UserViewModel result = new UserViewModel
+            {
+                Age = 23 + _rnd.Next(40),
+                Name = "Adam Yanukovich " + _rnd.Next(11),
+                Sex = "M",
+                FeedBackQuality = _rnd.Next(100),
+                AvatarId = id,
+                Id = id,
+                SkilGroups = CreateSkilGroups(3)
+            };
+
+            return View(result);
+        }
+
+        private SkilGroup[] CreateSkilGroups(int count)
+        {
+            return Enumerable.Range(0, count).Select(CreateSkilGroup).ToArray();
+        }
+
+        private SkilGroup CreateSkilGroup(int id)
+        {
+            return new SkilGroup
+            {
+                Name = "Skil group " + id,
+                Skils = Enumerable.Range(0, 2 + _rnd.Next(5)).Select(CreateSkil).ToArray()
+            };
+        }
+
+        private SkilInfo CreateSkil(int id)
+        {
+            return new SkilInfo
+            {
+                Name = "Skil info " + id,
+                Rate = _rnd.Next(11)
+            };
+        }
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+        public IActionResult SendFeedback()
         {
             return View();
         }
---
+++ RtfWebApp/Models/View/UserViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RtfWebApp.Models.View
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public int AvatarId { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+
+        public SkilGroup[] SkilGroups { get; set; }
+        /// <summary>
+        /// Качество обратной связи - насколько оценки этого пользователя актуальны
+        /// </summary>
+        public double FeedBackQuality { get; set; }
+    }
+
+    public class SkilGroup
+    {
+        public string Name { get; set; }
+
+        public SkilInfo[] Skils { get; set; }
+    }
+
+    public class SkilInfo
+    {
+        public string Name { get; set; }
+        public double Rate { get; set; }
+    }
+}

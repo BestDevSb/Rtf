3ab13e02a5a18d7b5c1691d6b43142f5ab6da549
Eugene Alekseev <e.v.alekseev@outlook.com>
2018-11-01 19:58:25 +0300

    Новая таблица: AuthorRatings

---
+++ RtfWebApp/Models/AuthorRating.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace RtfWebApp.Models
+{
+    [DataContract]
+    public class AuthorRating: IHaveId
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Кто ставит оценку
+        /// </summary>
+        [DataMember]
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+
+        [DataMember]
+        public DateTimeOffset Date { get; set; }
+
+        [DataMember]
+        public double Weight { get; set; }
+
+        [DataMember]
+        public int Rate { get; set; }
+
+        [DataMember]
+        [ForeignKey(nameof(Skill))]
+        public int SkillId { get; set; }
+
+        public Skil Skill { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
